7a731a1bfa7b15801e29833393211eb52c16f8fd
winderosion1git config --global user.email winderosion1@gmail.com <61790487+winderosion1@users.noreply.github.com>
2021-06-07 15:42:46 +0800

    Lab1

---
+++ src/main/java/com/example/DuQuanLin2019211003000234/lab1/Dog.java
@@ -0,0 +1,21 @@
+package com.example.DuQuanLin2019211003000234.lab1;
+
+public class Dog {
+     private String name ;
+
+     public Dog(){
+
+     }
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
